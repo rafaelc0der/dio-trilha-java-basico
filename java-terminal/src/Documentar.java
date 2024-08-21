8350de897e1a1bba38246cc0cf463623dc5bb017
Rafael Dias <rafael_dias_pb@hotmail.com>
2024-08-20 21:42:32 -0300

    JavaDoc - Aprendendo a documentar

---
+++ java-terminal/src/Documentar.java
@@ -0,0 +1,31 @@
+/**
+ * <h1>Calculadora</h1>
+ * A calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação dessa classe para
+ * desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Rafael Dias
+ * @version 1.0
+ * @since 20/08/2024
+ */
+
+public class Documentar {
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm primeiro parâmetro do método
+     * @param numeroDois segundo parâmetro do método
+     * @return returna a soma dos dois parâmetro
+     */
+
+    public int somar(int numeroUm, int numeroDois){
+          return numeroUm + numeroDois;
+   
+}
+}
+
+/*Criando documentação no formato HTML para disponibilzar na WEB
+No terminal, executar o comando:
+javadoc -encoding UTF-8 -docencoding ISO-8859-1 -d ../docs src/*.java
+ 
+ */
