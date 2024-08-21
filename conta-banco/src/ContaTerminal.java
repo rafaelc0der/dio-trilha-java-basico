b0edabf745e64dbaff85930ce785a6812631bddb
Rafael Dias <rafael_dias_pb@hotmail.com>
2024-08-21 18:44:46 -0300

    Sintaxe-Desafio-conta-banco

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da sua conta.");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite o número da sua agência.");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o seu nome.");
+        String nomeCliente = scanner.next();
+
+        System.out.println("Digite o seu saldo.");
+        double saldo = scanner.nextDouble();
+        scanner.close();
+
+        
+        
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + ", já está disponível para saque.");
+    }
+}
