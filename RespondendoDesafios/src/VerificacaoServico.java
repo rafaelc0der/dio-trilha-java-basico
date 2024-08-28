4fa43caedd2031427db4e7660ca7e3e5e7647d42
Rafael Dias <rafael_dias_pb@hotmail.com>
2024-08-28 09:48:39 -0300

    Desafios Explorando serviços de telefonia

---
+++ RespondendoDesafios/src/VerificacaoServico.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class VerificacaoServico {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Entrada do serviço a ser verificado
+        String servico = scanner.nextLine().trim();
+        
+        // Entrada do nome do cliente e os serviços contratados
+        String entradaCliente = scanner.nextLine().trim();
+        
+        // Separando o nome do cliente e os serviços contratados
+        String[] partes = entradaCliente.split(",");
+        String nomeCliente = partes[0];
+        boolean contratado = false;
+        
+        // TODO: Verifique se o serviço está na lista de serviços contratados
+        for (int nome = 1; nome < partes.length; nome++){
+          if(servico.equals(partes[nome])){
+            contratado = true;
+          }
+        }
+        
+        if(contratado == true){
+          System.out.println("Sim");
+        }else{
+          System.out.println("Nao");
+        }
+        scanner.close();
+    }
+}
